d3f5ec99fbe7e20e778efdce1dd6c576a481ae2c
citzeye <citzeye@gmail.com>
2026-02-21 21:18:34 +0700

    update

--- .config/scripts/r-all.sh
+++ .config/scripts/r-all.sh
@@ -3,22 +3,16 @@
 # --- 1. Update ZSHRC (Local Copy) ---
 cp /home/citz/loonix/.config/zshs/.zshrc ~/.zshrc
 
-# --- 2. Reload Wallpaper (Hyprpaper) ---
-# Tambahin & biar terminal gak nungguin
-pkill hyprpaper
-hyprpaper -c /home/citz/loonix/.config/hypr/hyprpaper.conf > /dev/null 2>&1 &
-
-# --- 3. Reload config Bar & Notif ---
-# Tambahin & biar terminal gak nungguin
+# --- 2. Reload config Bar & Notif ---
 pkill waybar
 waybar -c /home/citz/loonix/.config/waybar/config.jsonc -s /home/citz/loonix/.config/waybar/style.css > /dev/null 2>&1 &
 pkill dunst
 dunst &
 
-# --- 4. Reload config Hyprland ---
+# --- 3. Reload config Hyprland ---
 hyprctl reload
 
-# --- 5. Reload config Kitty ---
+# --- 4. Reload config Kitty ---
 pkill -USR1 kitty
 
 notify-send "Loonix" "Nuclear Refresh Done! 🚀"
